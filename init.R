e5b616dd9dcca6c9198f32249353e0d02f4f4380
dallaire44 <david.dallaire@sympatico.ca>
2017-09-03 08:19:41 -0400

    First commit

---
+++ init.R
@@ -0,0 +1,14 @@
+# init.R
+#
+# Example R code to install packages if not already installed
+#
+
+
+install.packages('devtools', repos='http://cran.us.r-project.org')
+devtools::install_github("RcppCore/Rcpp")
+devtools::install_github("rstats-db/DBI")
+devtools::install_github("rstats-db/RPostgres")
+install.packages('jsonlite', repos='http://cran.us.r-project.org')
+install.packages('httr', repos='http://cran.us.r-project.org')
+install.packages('reshape2', repos='http://cran.us.r-project.org')
+install.packages("opencpu")
