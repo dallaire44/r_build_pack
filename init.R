d9e8d20d5197be5cf05f93d982ff741f48badd7d
dallaire44 <david.dallaire@sympatico.ca>
2017-09-03 13:14:44 -0400

    Try sys only for package

--- init.R
+++ init.R
@@ -5,10 +5,5 @@
 
 
 install.packages('devtools', repos='http://cran.us.r-project.org')
-devtools::install_github("RcppCore/Rcpp")
-devtools::install_github("rstats-db/DBI")
-devtools::install_github("rstats-db/RPostgres")
-install.packages('jsonlite', repos='http://cran.us.r-project.org')
-install.packages('httr', repos='http://cran.us.r-project.org')
-install.packages('reshape2', repos='http://cran.us.r-project.org')
-install.packages("opencpu",dependencies=TRUE)
+install.packages('sys', repos='http://cran.us.r-project.org')
+
