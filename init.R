3c32d12e8ca38e2b3f5fafd2619eee7a4997fbc9
dallaire44 <david.dallaire@sympatico.ca>
2017-09-04 11:43:57 -0400

    system cmd2

--- init.R
+++ init.R
@@ -3,8 +3,11 @@
 # Example R code to install packages if not already installed
 #
 
+system("echo $PATH")
 
 system("export PATH=$PATH:/app/.root/usr/bin/pkg-config")
+
+system("echo $PATH")
 
 install.packages("protolite", repos='http://cran.us.r-project.org',configure.vars=c(INCLUDE_DIR="/app/vendor/protobuf/include/google/protobuf",LIB_DIR="/app/vendor/protobuf/lib"))
 
