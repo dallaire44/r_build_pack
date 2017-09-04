97009f36fa7edfd077d2ea860d2732d76952bf07
dallaire44 <david.dallaire@sympatico.ca>
2017-09-04 10:01:25 -0400

    old init

--- init.R
+++ init.R
@@ -4,5 +4,12 @@
 #
 
 
-install.packages("protolite")
+install.packages('devtools', repos='http://cran.us.r-project.org')
+devtools::install_github("RcppCore/Rcpp")
+devtools::install_github("rstats-db/DBI")
+devtools::install_github("rstats-db/RPostgres")
+install.packages('jsonlite', repos='http://cran.us.r-project.org')
+install.packages('httr', repos='http://cran.us.r-project.org')
+install.packages('reshape2', repos='http://cran.us.r-project.org')
+install.packages("protolite", repos='http://cran.us.r-project.org')
 
