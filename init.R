e83bb60d908b1679d8b76c71138203c077409dc3
dallaire44 <david.dallaire@sympatico.ca>
2017-09-04 16:54:40 -0400

    hopefully last last one with opencpu

--- init.R
+++ init.R
@@ -6,7 +6,7 @@
 
 #install.packages("protolite", repos='http://cran.us.r-project.org',configure.vars=c(INCLUDE_DIR="/app/vendor/protobuf/include/google/protobuf",LIB_DIR="/app/vendor/protobuf/lib"))
 
-install.packages('devtools', repos='http://cran.us.r-project.org')
-devtools::install_github("RcppCore/Rcpp")
+#install.packages('devtools', repos='http://cran.us.r-project.org')
+#devtools::install_github("RcppCore/Rcpp")
 install.packages("/app/protolite_1.7.dd.tar.gz",repos=NULL, type="source")
 install.packages("opencpu",repos='http://cran.us.r-project.org')
