f0c360108ec314d50ea3bbe48f71d00037887201
dallaire44 <david.dallaire@sympatico.ca>
2017-09-04 17:37:08 -0400

    hopefully last last one with opencpu

--- init.R
+++ init.R
@@ -8,7 +8,7 @@
 
 #install.packages('devtools', repos='http://cran.us.r-project.org')
 install.packages('devlools')
-#devtools::install_github("RcppCore/Rcpp")
+devtools::install_github("RcppCore/Rcpp")
 install.packages("/app/protolite_1.7.dd.tar.gz",repos=NULL, type="source")
 #install.packages("opencpu",repos='http://cran.us.r-project.org',dependencies=FALSE)
 install.packages("stringi")
